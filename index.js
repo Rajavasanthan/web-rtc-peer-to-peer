1839c26d6e26c38b3306ab9f3994ac87edb26c9d
rajavasanthan <Rajavasanthan@gmail.com>
2020-07-01 21:59:47 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+navigator.webkitGetUserMedia(
+  { video: true, audio: true },
+  function (stream) {
+    var Peer = require("simple-peer");
+    var peer = new Peer({
+      initiator: location.hash == "#init",
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", function (data) {
+      console.log(data);
+      document.getElementById("myId").value = JSON.stringify(data);
+    });
+
+    document.getElementById("connect").addEventListener("click", function () {
+      var otherId = JSON.parse(document.getElementById("otherId").value);
+      console.log(otherId);
+      peer.signal(otherId);
+    });
+
+    document.getElementById("post").addEventListener("click", function () {
+      var message = document.getElementById("message").value;
+      peer.send(message);
+    });
+
+    peer.on("data", function (data) {
+      document.getElementById("messages").textContent += data + "\n";
+    });
+
+    peer.on("stream", function (stream) {
+      console.log("Streaming");
+      let myVidePlayer = document.createElement("video");
+      document.body.appendChild(myVidePlayer);
+      var mediaStream = new MediaStream(stream);
+      myVidePlayer.srcObject = mediaStream;
+      myVidePlayer.play();
+    });
+  },
+  function (err) {
+    console.log(err);
+  }
+);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+module.exports = {
+  entry: path.join(__dirname, "/index.js"),
+  output: {
+    filename: "script.js",
+    path: __dirname,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
