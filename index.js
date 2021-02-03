7db91d4323538b989a9401a06f3d9c39321282d2
rajavasanthan <rajavasanthan@gmail.com>
2021-02-03 23:36:03 +0530

    Socket connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 navigator.webkitGetUserMedia(
   { video: true, audio: true },
   function (stream) {
+    
     var Peer = require("simple-peer");
     var peer = new Peer({
       initiator: location.hash == "#init",
@@ -11,18 +12,25 @@ navigator.webkitGetUserMedia(
     peer.on("signal", function (data) {
       console.log(data);
       document.getElementById("myId").value = JSON.stringify(data);
+      socket.emit('passWEBRTC', {data});
     });
 
-    document.getElementById("connect").addEventListener("click", function () {
-      var otherId = JSON.parse(document.getElementById("otherId").value);
-      console.log(otherId);
-      peer.signal(otherId);
-    });
+    // document.getElementById("connect").addEventListener("click", function () {
+    //   var otherId = JSON.parse(document.getElementById("otherId").value);
+    //   console.log(otherId);
+    //   peer.signal(otherId);
+    // });
 
     document.getElementById("post").addEventListener("click", function () {
       var message = document.getElementById("message").value;
       peer.send(message);
     });
+
+    socket.on("reciveWEBRTC",function(data){
+      console.log(JSON.stringify(data.data))
+      document.getElementById("otherId").value = JSON.stringify(data.data)
+      peer.signal(JSON.stringify(data.data));
+    })
 
     peer.on("data", function (data) {
       document.getElementById("messages").textContent += data + "\n";
